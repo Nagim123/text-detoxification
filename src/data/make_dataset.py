718a0e753de84485edb0caccde09dccbd0c44983
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-09-27 23:49:11 +0300

    Add starting notebook

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,10 +1,13 @@
 import requests
 import pandas as pd
+import os
 
 class BaseDatasetMaker():
     def __init__(self, output_dir: str, file_path: str = None) -> None:
         if file_path == None:
             file_path = self.download_data()
+        if not os.path.exists(file_path):
+            raise Exception(f"Cannot find path {file_path}")
         # Set path to file with raw data
         self.file_path = file_path
         # Parse raw data into meaningful structure
@@ -15,12 +18,19 @@ class BaseDatasetMaker():
         extracted_text.to_csv(output_dir)
 
     def download_data(self) -> str:
-        pass
+        raise Exception("Not implemented")
 
     def parse_data(self):
-        pass
+        raise Exception("Not implemented")
+    
     def extract_toxic_and_detoxified_text(self) -> pd.DataFrame:
-        pass
+        raise Exception("Not implemented")
 
 class ParaNMTDetoxDatasetMaker(BaseDatasetMaker):
-    pass
+
+    def download_data(self) -> str:
+        url = "https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip"
+        response = requests.get(url)
+        data_file = open("filtered_paranmt.zip", "wb")
+        data_file.write(response.content)
+
