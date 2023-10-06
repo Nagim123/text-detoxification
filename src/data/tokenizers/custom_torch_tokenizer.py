ea8f3759140c47b28fc02213b90df7050b48d780
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-06 15:43:54 +0300

    Add max word frequency

--- src/data/tokenizers/custom_torch_tokenizer.py
+++ src/data/tokenizers/custom_torch_tokenizer.py
@@ -8,7 +8,7 @@ class CustomTorchTokenizer(BasicTokenizer):
     def create_vocab(self, full_text: list[str]) -> None:
         full_tokenized_text = [nltk.tokenize.word_tokenize(text) for text in full_text]
         special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
-        self.vocab = build_vocab_from_iterator(full_tokenized_text, specials=special_symbols)
+        self.vocab = build_vocab_from_iterator(full_tokenized_text, specials=special_symbols, min_freq=2)
         self.vocab.set_default_index(0)
 
     
