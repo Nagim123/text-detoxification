c9f7b1ff658b7e2b7389bcbfa31564603c2cc12d
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-07 18:59:14 +0300

    Finish transformers

---
+++ src/data/make_dataset.py
@@ -0,0 +1,72 @@
+from zipfile import ZipFile
+from tqdm import tqdm
+from torchtext.vocab import build_vocab_from_iterator
+from torch.utils.data import Dataset
+import torch
+import logging
+import argparse
+import spacy
+import pandas as pd
+import pathlib
+import os
+
+SCRIPT_PATH = pathlib.Path(__file__).parent.resolve()
+DATASET_PATH = os.path.join(SCRIPT_PATH, "../../data/raw/filtered_paranmt.zip")
+OUTPUT_DIR_PATH = os.path.join(SCRIPT_PATH, "../../data/interim")
+
+def read_dataset() -> pd.DataFrame:
+    zip_object = ZipFile(DATASET_PATH)
+    zip_object.extractall(OUTPUT_DIR_PATH)
+    zip_object.close()
+
+    tsv_file_path = os.path.join(OUTPUT_DIR_PATH, "filtered.tsv")
+    dataset_df = pd.read_csv(tsv_file_path, sep='\t')
+    os.remove(tsv_file_path)
+    return dataset_df
+
+def parse_dataset(dataset_df: pd.DataFrame) -> tuple[list[str], list[str]]:
+    parsed_df = pd.DataFrame()
+    parsed_df["toxic"] = dataset_df.apply(lambda row: row["reference"].lower() if row["ref_tox"] > row["trn_tox"] else row["translation"].lower(), axis=1)
+    parsed_df["detoxified"] = dataset_df.apply(lambda row: row["translation"].lower() if row['ref_tox'] > row['trn_tox'] else row['reference'].lower(), axis=1)
+    parsed_df['detoxified_tox'] = dataset_df.apply(lambda row: min(row['ref_tox'], row['trn_tox']), axis=1)
+    parsed_df['toxic_tox'] = dataset_df.apply(lambda row: max(row['ref_tox'], row['trn_tox']), axis=1)
+    
+    rows_to_drop = parsed_df[(parsed_df['detoxified_tox'] > 0.2) | (parsed_df['toxic_tox'] < 0.8)]
+    balanced_df = parsed_df.drop(rows_to_drop.index)
+    balanced_df.reset_index(inplace=True, drop=True)
+    return balanced_df["toxic"].tolist(), balanced_df["detoxified"].tolist()
+
+def tokenize_texts(texts: list[str], do_logging: bool = False):
+    spacy_eng = spacy.load("en_core_web_sm")
+    tokenized_texts = []
+    if do_logging:
+        texts = tqdm(texts)
+    for text in texts:
+        tokenized_texts.append([tok.text for tok in spacy_eng.tokenizer(text)])
+    return tokenized_texts
+
+def tokenize_text(text: list[str]):
+    spacy_eng = spacy.load("en_core_web_sm")
+    return [tok.text for tok in spacy_eng.tokenizer(text)]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--logging", action="store_true")
+    args = parser.parse_args()
+
+    if args.logging:
+        logging.basicConfig(level=logging.INFO)
+    
+    logging.info("Reading dataset...")
+    df = read_dataset()
+    logging.info("Parsing dataset...")
+    toxic, detoxified = parse_dataset(df)
+    
+    logging.info("Tokenization for toxic text...")
+    tokenized_toxic = tokenize_texts(toxic, args.logging)
+    logging.info("Tokenization for detoxified text...")
+    tokenized_detoxified = tokenize_texts(detoxified, args.logging)
+    
+    logging.info("Saving on disk...")
+    torch.save({"toxic": tokenized_toxic, "detoxified": tokenized_detoxified}, os.path.join(OUTPUT_DIR_PATH, "dataset.pt"))
+    logging.info("Done!")
---
+++ src/models/train_model.py
@@ -0,0 +1,165 @@
+import torch
+import pathlib
+import os
+import argparse
+from torch.utils.data import random_split, Dataset, DataLoader
+from torchtext.vocab import build_vocab_from_iterator
+from torchtext.data.metrics import bleu_score
+from tqdm import tqdm
+from torch import nn
+from torch import optim
+
+SCRIPT_PATH = pathlib.Path(__file__).parent.resolve()
+DATASET_PATH = os.path.join(SCRIPT_PATH, "../../data/interim/dataset.pt")
+MODEL_WEIGHTS_PATH = os.path.join(SCRIPT_PATH, "../../models/weights.pt")
+MAX_SENTENCE_SIZE = 255
+PAD_IDX = 1
+
+def bleu(model, src, trg, vocab, device):
+    model.eval()
+    with torch.no_grad():
+        output = model(src, trg[:-1]).to(device)
+        output = output.permute([1, 0, 2])
+        trg = trg.permute([1, 0, 2])
+        token_list1 = []
+        token_list2 = []
+        idx2word = vocab.get_itos()
+        for t1, t2 in zip(output, trg):
+            t1 = torch.max(output, axis=1)
+            t1, t2 = t1.tolist(), t2.tolist()
+            token_list1.append([idx2word[t] for t in t1])
+            token_list2.append([idx2word[t] for t in t2])
+        return bleu_score(token_list1, token_list2)
+
+
+
+class DetoxificationModel(nn.Module):
+    def __init__(self, embedding_size, vocab_size, dropout, max_len, device):
+        super(DetoxificationModel, self).__init__()
+        self.word_embedding = nn.Embedding(vocab_size, embedding_size)
+        self.position_embedding = nn.Embedding(max_len, embedding_size)
+        self.device = device
+        self.transformer = nn.Transformer(embedding_size, 8, 3, 3, 4, dropout, batch_first=False)
+        self.fc_out = nn.Linear(embedding_size, vocab_size)
+        self.dropout = nn.Dropout(dropout)
+        self.src_pad_idx = PAD_IDX
+    
+    def make_src_mask(self, src):
+        src_mask = src.transpose(0, 1) == self.src_pad_idx
+        return src_mask
+    
+    def forward(self, src, trg):
+        src_seq_len, N = src.shape
+        trg_seq_len, N = trg.shape
+
+        src_positions = (
+            torch.arange(0, src_seq_len).unsqueeze(1).expand(src_seq_len, N)
+            .to(self.device)
+        )
+        trg_positions = (
+            torch.arange(0, trg_seq_len).unsqueeze(1).expand(trg_seq_len, N)
+            .to(self.device)
+        )
+        embed_src = self.dropout(
+            (self.word_embedding(src) + self.position_embedding(src_positions))
+        )
+        embed_trg = self.dropout(
+            (self.word_embedding(trg) + self.position_embedding(trg_positions))
+        )
+        src_padding_mask = self.make_src_mask(src)
+        trg_mask = self.transformer.generate_square_subsequent_mask(trg_seq_len, self.device)
+        out = self.transformer(
+            embed_src,
+            embed_trg,
+            src_key_padding_mask = src_padding_mask,
+            tgt_mask = trg_mask
+        )
+        out = self.fc_out(out)
+        return out
+
+class ToxicTextDataset(Dataset):
+    def __init__(self, toxic_texts: list[str], detoxified_texts: list[str]):
+        self.toxic_texts = toxic_texts
+        self.detoxified_texts = detoxified_texts
+        self.special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+        self.vocab = build_vocab_from_iterator(toxic_texts + detoxified_texts, special_first=self.special_symbols, max_tokens=10000, min_freq=2)
+        self.vocab.set_default_index(0)
+
+    def __getitem__(self, index: int) -> tuple[list, list]:
+        return self.vocab(self.toxic_texts[index]), self.vocab(self.detoxified_texts[index])
+    
+    def __len__(self):
+        return len(self.toxic_texts)
+
+def train_one_epoch(model, train_loader, optmizer, loss_fn):
+    model.train()
+    progress = tqdm(train_loader)
+    for batch in progress:
+        input, target = batch
+        input, target = input.to(device), target.to(device)
+        output = model(input, target[:-1])
+        output = output.reshape(-1, output.shape[2])
+        target = target[1:].reshape(-1)
+        optmizer.zero_grad()
+        
+        loss = loss_fn(output, target)
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+        optmizer.step()
+        progress.set_postfix({"loss":loss.item()})
+
+def val_one_epoch(model, val_loader, loss_fn):
+    model.eval()
+    progress = tqdm(val_loader)
+    with torch.no_grad():
+        for batch in progress:
+            input, target = batch
+            input, target = input.to(device), target.to(device)
+
+            output = model(input, target[:-1])
+            output = output.reshape(-1, output.shape[2])
+            target = target[1:].reshape(-1)
+            
+            loss = loss_fn(output, target)
+            progress.set_postfix({"loss":loss.item()})
+    
+
+def collate_batch(batch: list):
+    max_size = MAX_SENTENCE_SIZE
+    _toxic_batch, _detoxic_batch = [], []
+    for _toxic, _detoxic in batch:
+        _toxic = _toxic[:max_size] if len(_toxic) > max_size else _toxic + [PAD_IDX]*(max_size - len(_toxic))
+        _detoxic = _detoxic[:max_size] if len(_detoxic) > max_size else _detoxic + [PAD_IDX]*(max_size - len(_detoxic))
+        _toxic_batch.append(torch.tensor(_toxic))
+        _detoxic_batch.append(torch.tensor(_detoxic))
+    
+    _toxic_batch = torch.stack(_toxic_batch, dim=0)
+    _detoxic_batch = torch.stack(_detoxic_batch, dim=0)
+    return _toxic_batch.T, _detoxic_batch.T
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--epochs", default=5)
+    parser.add_argument("--batch_size", default=32)
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    text_data = torch.load(DATASET_PATH)
+    toxic_text_dataset = ToxicTextDataset(text_data["toxic"], text_data["detoxified"])
+    train_dataset, val_dataset = random_split(toxic_text_dataset, [0.7, 0.3])
+    vocab_size = len(toxic_text_dataset.vocab)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_batch)
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_batch)
+
+    model = DetoxificationModel(512, vocab_size, 0.1, MAX_SENTENCE_SIZE, device).to(device)
+    optmizer = optim.Adam(model.parameters(), 3e-4)
+    loss_fn = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
+
+    for epoch in range(args.epochs):
+        train_one_epoch(model, train_dataloader, optmizer, loss_fn)
+        val_one_epoch(model, val_dataloader, loss_fn)
+        print("SAVE MODEL")
+        torch.save(model.state_dict(), MODEL_WEIGHTS_PATH)
