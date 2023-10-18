501af0a7c0d2507729051d76dcde1b4fd01bb84e
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-18 13:37:42 +0300

    Add metrics calculation pt1

--- src/data/download_weights.py
+++ src/data/download_weights.py
@@ -1,20 +1,29 @@
 import gdown
 import os
-
 from constants import WEIGHTS_DIR
 
-def download_model_weights(url: str, output_dir: str):
+def download_model_weights(url: str, output_dir: str) -> None:
+    """
+    Download weights from internet.
+
+    Parameters:
+        url (str): URL for weight downloading.
+        output_dir (str): Path where to save weight file.
+    """
     gdown.download(url, output_dir, quiet=False, fuzzy=True)
 
+# Web links to download weights
 lstm_url = "https://drive.google.com/file/d/17kWTfP9WOcWKsK_0wXfYYzThbHdbK9lb/view?usp=share_link"
 ae_lstm_url = "https://drive.google.com/file/d/1I4oFZWK7qecLEiTlPuD_euMn89WY_emp/view?usp=share_link"
 transformer_url = "https://drive.google.com/file/d/1_EY9jxsJCnSleOaDqD1bT_jSC2DBct19/view?usp=share_link"
 
+# Path where to save weights
 lstm_path = os.path.join(WEIGHTS_DIR, "lstm.pt")
 ae_lstm_path = os.path.join(WEIGHTS_DIR, "ae_lstm.pt")
 transformer_path = os.path.join(WEIGHTS_DIR, "transformer.pt")
 
 if __name__ == "__main__":
+    # Downloading all weights for custom models
     print("Start downloading model's weights...")
     download_model_weights(lstm_url, lstm_path)
     download_model_weights(ae_lstm_url, ae_lstm_path)
---
+++ src/models/core/utils/metrics_calculator.py
@@ -0,0 +1,21 @@
+from .text_manager import TextManager
+from ..metrics import rouges_metric, ter_metric, bleu_metric
+
+def generate_metric_evalulation(predictions: list[str], text_manager: TextManager):
+
+    report_data = []
+    for i in range(len(text_manager)):
+        target, original = text_manager.get_raw_detoxified(i), text_manager.get_raw(i)
+        prediction = predictions[i]
+
+        evaluation_info = {
+            "generated": prediction,
+            "original": original,
+            "true_translated": target,
+            "BLEU": bleu_metric.calculate_bleu(original, prediction),
+            "TER score": ter_metric.calculate_ter(original, prediction),
+            "ROUGES": rouges_metric.calculate_rogues(original, prediction)
+        }
+
+        report_data.append(evaluation_info)
+    return report_data
