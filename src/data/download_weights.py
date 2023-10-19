62f4bc5fc5ce01c75714b6075f30113d8e907d9c
Nagim <39875672+Nagim123@users.noreply.github.com>
2023-10-19 10:28:24 +0300

    Update links

--- src/data/download_weights.py
+++ src/data/download_weights.py
@@ -13,9 +13,9 @@ def download_model_weights(url: str, output_dir: str) -> None:
     gdown.download(url, output_dir, quiet=False, fuzzy=True)
 
 # Web links to download weights
-lstm_url = "https://drive.google.com/file/d/17kWTfP9WOcWKsK_0wXfYYzThbHdbK9lb/view?usp=share_link"
+lstm_url = "https://drive.google.com/file/d/1ZD3Fi51Cmf_lrvlTlUlx0ZTKoWooLkXD/view?usp=share_link"
 ae_lstm_url = "https://drive.google.com/file/d/1I4oFZWK7qecLEiTlPuD_euMn89WY_emp/view?usp=share_link"
-transformer_url = "https://drive.google.com/file/d/1_EY9jxsJCnSleOaDqD1bT_jSC2DBct19/view?usp=share_link"
+transformer_url = "https://drive.google.com/file/d/1ilGwJUWX5KKk6caIzPI9wSAoBQ186slv/view?usp=share_link"
 
 # Path where to save weights
 lstm_path = os.path.join(WEIGHTS_DIR, "lstm.pt")
